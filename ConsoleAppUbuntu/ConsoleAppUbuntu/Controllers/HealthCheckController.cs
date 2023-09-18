f5d1c2cd6fb9c42f72e6bc779938861baf38ba8d
biro224 <boubacarbirobarry837@gmail.com>
2023-09-18 23:09:32 +0300

    book sotre commit

---
+++ ConsoleAppUbuntu/ConsoleAppUbuntu/Controllers/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleAppUbuntu.Controllers
+{
+    [ApiController]
+    [Route("/HC")]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet("healthCheck")]
+        public ActionResult HealthCheck()
+        {
+            return Ok();
+        }
+    }
+}
